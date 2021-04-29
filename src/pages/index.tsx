4f18e8acb823e64bbda37794805c8217eeb6a5a7
EddyPBR <edvaldojunior1310@gmail.com>
2021-04-29 15:13:22 -0300

    :octocat: Iniciando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Fala Eddy o/</h1>
+      <p>
+        Deixou de procrastinar e finalmente vai criar o seu website? -
+        finalmente ein! mãos na massa!
+      </p>
+    </main>
+  );
+}
