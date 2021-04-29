7fe7b5091291ad0c11f5a9bb8e2ce8b3bf286a88
EddyPBR <edvaldojunior1310@gmail.com>
2021-04-29 17:00:20 -0300

    :package: Adicionando inicio da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,41 @@
+import styles from "./home.module.scss";
+
+import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Fala Eddy o/</h1>
-      <p>
-        Deixou de procrastinar e finalmente vai criar o seu website? -
-        finalmente ein! mãos na massa!
-      </p>
-    </main>
+    <div className={styles.presentation}>
+      <main>
+        
+        <div>
+          <p>Olá, me chamo</p>
+          <h1>Edvaldo Junior,</h1>
+          <h2>Sou um desenvolvedor fullstack web e mobile, este é o meu site, espero que goste!</h2>
+          
+          <ul className={styles.socialLinks}>
+            <li>
+              <a href="https://github.com/EddyPBR/" rel="noopener noreferrer" target="_blank">
+                <FiGithub size={24} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/edvaldo_junior_dev/" rel="noopener noreferrer" target="_blank">
+                <FiInstagram size={24} />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/EddyPBR" rel="noopener noreferrer" target="_blank">
+                <FiLinkedin size={24} />
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <img src="./assets/desenvolvedor-e-computador.svg" alt="EddyPBR" />
+        </div>
+
+      </main>
+    </div>
   );
 }
