95c35a2b578b8e88f159324645331097b56c779d
EddyPBR <edvaldojunior1310@gmail.com>
2021-04-30 13:16:50 -0300

    :package: Definindo responsivadade na seção de habilidades

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,9 +34,17 @@ export default function Home() {
       <section className={styles.skill}>
         <h1>Minhas Habilidades</h1>
         <div>
-          <SkillCard headerImage="./assets/server-ilustration.svg" title="API REST" text="Desenvolvimento de sistemas que podem ser integrados a interfaces ou não, como sistema de envio de emails." footerImage="./assets/typescript-nodejs.svg" />
-          <SkillCard headerImage="./assets/website-ilustration.svg" title="WEBSITES" text="Websites para negócios ou interfaces via navegador para sistemas internos e tudo com responsividade." footerImage="./assets/react-nextjs.svg" />
-          <SkillCard headerImage="./assets/server-ilustration.svg" title="APP's" text="Aplicativos mobile que podem ou não serem integrados com uma API, criando então uma interface mobile para o sistema." footerImage="./assets/react-native.svg" />
+          <ul className={styles.listSkills}>
+            <li>
+              <SkillCard headerImage="./assets/server-ilustration.svg" title="API REST" text="Desenvolvimento de sistemas que podem ser integrados a interfaces ou não, como sistema de envio de emails." footerImage="./assets/typescript-nodejs.svg" />
+            </li>
+            <li>
+              <SkillCard headerImage="./assets/website-ilustration.svg" title="WEBSITES" text="Websites para negócios ou interfaces via navegador para sistemas internos e tudo com responsividade." footerImage="./assets/react-nextjs.svg" />
+            </li>
+            <li>
+              <SkillCard headerImage="./assets/server-ilustration.svg" title="APP's" text="Aplicativos mobile que podem ou não serem integrados com uma API, criando então uma interface mobile para o sistema." footerImage="./assets/react-native.svg" />
+            </li>
+          </ul>
         </div>
       </section>
     </>
