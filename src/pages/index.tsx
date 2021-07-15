e8e6e9646359218af1f4ecaba20fe674c130d29d
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-15 14:49:49 -0300

    :package: Adicionando seção de services

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,27 +2,42 @@ import Head from "next/head";
 
 import { Navbar } from "../components/Navbar";
 
-import { Header, Container } from "../styles/Home";
+import { Header, Services } from "../styles/Home";
 
 export default function Home() {
   return (
-    <Header>
+    <>
       <Head>
         <title>Início | EddyPBR</title>
       </Head>
-      <Container>
-        <main>
-          <h1>
-            <span>Olá, me chamo</span>
-            Edvaldo Junior
-          </h1>
-          <p>
-            Desenvolvedor fullstack contruindo sistemas com servidores, websites e aplicativos mobile todos integrando um único sistema
-          </p>
-        </main>
+      
+      <Header>
+        <div>
+          <main>
+            <h1>
+              <span>Olá, me chamo</span>
+              Edvaldo Junior
+            </h1>
+            <p>
+              Desenvolvedor fullstack contruindo sistemas com servidores, websites e aplicativos mobile todos integrando um único sistema
+            </p>
+          </main>
+          <Navbar />
+        </div>
+      </Header>
 
-        <Navbar />
-      </Container>
-    </Header>
+      <Services>
+        <div />
+        <article>
+          <h1>Meus serviços</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultrices 
+            dui nec mi vehicula rhoncus ut a risus. Sed placerat tellus ut mi rhoncus, 
+            sit amet consectetur enim egestas. Nulla mi diam, posuere non tincidunt id, 
+            accumsan sed metus. Nullam feugiat eros at odio pretium dictum.
+          </p>
+        </article>
+      </Services>
+    </>
   );
 }
--- src/styles/Home.tsx
+++ src/styles/Home.tsx
@@ -7,30 +7,60 @@ export const Header = styled.header`
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-`;
 
-export const Container = styled.div`
-  width: 100%;
-  max-width: 97.0rem;
-  margin: 0 auto;
+  > div {
+    width: 90vw;
+    max-width: 97.0rem;
+    margin: 0 auto;
 
-  > main {
-    width: 100%;
-    max-width: 37.2rem;
-    padding-top: 12.0rem;
+    > main {
+      width: 100%;
+      max-width: 37.2rem;
+      padding-top: 12.0rem;
 
-    span {
-      display: block;
-      color: ${props => props.theme.colors.headingLight};
-      font: ${props => props.theme.fonts.subtitle};
+      span {
+        display: block;
+        color: ${props => props.theme.colors.headingLight};
+        font: ${props => props.theme.fonts.subtitle};
+      }
+
+      p {
+        font-family: "Nunito", sans-serif;
+      }
     }
 
-    p {
-      font-family: "Nunito", sans-serif;
+    > nav {
+      margin-top: 4.8rem;
     }
   }
+`;
 
-  > nav {
-    margin-top: 4.8rem;
+export const Services = styled.section`
+  width: 90vw;
+  max-width: 117rem;
+  margin: 0 auto;
+  margin-top: -28rem;
+
+  display: flex;
+  align-items: flex-start;
+  gap: 3.0rem;
+
+  > div {
+    width: 100%;
+    max-width: 27rem;
+    height: 0.4rem;
+    background-color: ${props => props.theme.colors.primary};
+    border-radius: 999px;
+    margin-top: 3rem
+  }
+
+  > article {
+    width: 100%;
+    max-width: 37rem;
+
+    > p {
+      line-height: 2.8rem;
+      margin-left: 0.4rem;
+    }
   }
 `;
