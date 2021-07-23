b144cc4a694438d8ce4d048e0b2e3d24e7690caa
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-23 16:39:29 -0300

    :package: Página portfolio retornando os projetos estáticamente

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -2,81 +2,26 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
+import { projects as projectData, ProjectType } from "../data/projects";
+
 import { DropdownProject } from "../components/DropdownProject";
 import { BackToHomeLink } from "../components/BackToHomeLink";
 
 import { Container, ProjectsBox, ActiveProject, Blobs } from "../styles/Portfolio";
 
-type FetchedProject = {
-  title: string,
-  category: string,
-  abstract: string,
-  icon: string,
-  thumbnail: string,
-  url: string,
-}
-
-type Project = FetchedProject & {
+type Project = ProjectType & {
   isActive: boolean;
 }
 
-export default function Portfolio() {
-  const fetchProjects: FetchedProject[] = [
-    {
-      title: "Letmeask",
-      category: "web | pwa",
-      abstract: "Sistema de perguntas e respostas com ranqueamento de likes, excelente para responder perguntas em lives!",
-      icon: "./projects/letmeask/letmeask-icon.jpg",
-      thumbnail: "./projects/letmeask/letmeask-thumbnail.jpg",
-      url: "#",
-    },
-    {
-      title: "Gameplay",
-      category: "mobile",
-      abstract: "Agende jogatinas com seus amigos com integração via discord app!",
-      icon: "./projects/gameplay/gameplay-icon.jpg",
-      thumbnail: "./projects/gameplay/gameplay-thumbnail.jpg",
-      url: "#",
-    },
-    {
-      title: "Move-it",
-      category: "web",
-      abstract: "Técnica de pomodoro com desafios de relaxamento corporais e oculares em um sistema de leveling up!",
-      icon: "./projects/moveit/moveit-icon.jpg",
-      thumbnail: "./projects/moveit/moveit-thumbnail.jpg",
-      url: "#",
-    },
-    {
-      title: "Mural digital",
-      category: "api | web | mobile",
-      abstract: "Um sistema para informar e alertar anúncios e notícias para os funcionários da empresa!",
-      icon: "./projects/muraldigital/muraldigital-icon.jpg",
-      thumbnail: "./projects/muraldigital/muraldigital-thumbnail.jpg",
-      url: "#"
-    },
-    {
-      title: "Codebacker",
-      category: "mobile",
-      abstract: "Um aplicativo para controle do carregamento de produtos em veículos empresariais!",
-      icon: "./projects/codebacker/codebacker-icon.jpg",
-      thumbnail: "./projects/codebacker/codebacker-thumbnail.jpg",
-      url: "#"
-    }
-  ];
-
-  const [projects, setProjects] = useState<Project[]>([]);
+export default function Portfolio({ projects: projectData }) {
+  const [projects, setProjects] = useState<Project[]>(projectData);
   const [activeProject, setActiveProject] = useState<Project | undefined>();
   const [animate, setAnimate] = useState(true);
 
   useEffect(() => {
-    if(!fetchProjects) {
+    if(!projectData) {
       return;
     }
-
-    const projects: Project[] = fetchProjects.map((project) => {
-      const newProject = Object.assign({ isActive: false }, project);
-      return newProject;
-    });
 
     projects[0].isActive = true;
     setActiveProject(projects[0]);
@@ -84,8 +29,6 @@ export default function Portfolio() {
     setTimeout(() => { 
       setAnimate(false);
     }, 1000);
-
-    setProjects(projects);
   }, []);
 
   function handleActiveProject(key: number) {
@@ -143,4 +86,17 @@ export default function Portfolio() {
       <BackToHomeLink />
     </>
   )
+}
+
+export function getStaticProps() {
+  const projects: Project[] = projectData.map((project) => {
+    const newProject = Object.assign({ isActive: false }, project);
+    return newProject;
+  });
+
+  return {
+    props: {
+      projects,
+    }
+  }
 }
