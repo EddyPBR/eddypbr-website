cba5ced0156c709f734c534e01a956127ab7c06b
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-26 10:16:08 -0300

    :wrench: Redefinindo tipagem do theme.d.ts

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,32 +1,9 @@
 import "styled-components";
 
+import theme from "./theme"
+
+export type Theme = typeof theme
+
 declare module "styled-components" {
-  export interface DefaultTheme {
-    colors: {
-      primaryDark: string;
-      primary: string;
-      primaryLight: string;
-
-      secondaryDark: string;
-      secondary: string;
-      secondaryLight: string;
-
-      headingDark: string;
-      heading: string;
-      headingLight: string;
-
-      black: string;
-      black600: string,
-      black700: string,
-      white: string;
-
-      background: string;
-    },
-    fonts: {
-      title: string;
-      subtitle: string;
-      text: string;
-      bold: string;
-    },
-  }
+  export interface DefaultTheme extends Theme {};
 }
