0a51a9e0adac610299d3cac976fc0fc253696667
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-26 10:16:57 -0300

    :package: Habilitando styled-components com server side

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,41 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentInitialProps, DocumentContext, Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  render() {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): JSX.Element {
     return (
-      <Html>
+      <Html lang="pt">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <meta name="theme-color" content="#645CEE"/>
+          <meta name="theme-color" content="#645CEE" />
         </Head>
         <body>
           <Main />
