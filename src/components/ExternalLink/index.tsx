e5cd7d4a0ff29486c07f1c1cac63dc3c91cb9207
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-27 13:13:13 -0300

    :wrench: Alterando component externalLink

--- src/components/ExternalLink/index.tsx
+++ src/components/ExternalLink/index.tsx
@@ -1,31 +1,24 @@
 import { Link } from "./styles";
 
-import { FaSitemap, FaGithub, FaLinkedinIn, FaGooglePlay, FaApple, FaFacebookF, FaInstagram, FaYoutube, FaTwitter} from "react-icons/fa";
+import { FaSitemap, FaGithub, FaLinkedinIn, FaGooglePlay, FaApple, FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa";
 
 type ExternalLinkProps = {
-  website?: string;
-  github?: string;
-  linkedin?: string;
-  googlePlay?: string;
-  appleStore?: string;
-  facebook?: string;
-  instagram?: string;
-  youtube?: string;
-  twitter?: string;
+  type: string;
+  url: string;
 }
 
-export function ExternalLink(props: ExternalLinkProps) {
-  return(
-    <>
-      { props.website && <Link href={props.website} rel="noopener noreferrer" target="_blank"><FaSitemap /></Link> }
-      { props.github && <Link href={props.github} rel="noopener noreferrer" target="_blank"><FaGithub /></Link> }
-      { props.linkedin && <Link href={props.linkedin} rel="noopener noreferrer" target="_blank"><FaLinkedinIn /></Link> }
-      { props.googlePlay && <Link href={props.googlePlay} rel="noopener noreferrer" target="_blank"><FaGooglePlay /></Link> }
-      { props.appleStore && <Link href={props.appleStore} rel="noopener noreferrer" target="_blank"><FaApple /></Link> }
-      { props.facebook && <Link href={props.facebook} rel="noopener noreferrer" target="_blank"><FaFacebookF /></Link> }
-      { props.instagram && <Link href={props.instagram} rel="noopener noreferrer" target="_blank"><FaInstagram /></Link> }
-      { props.youtube && <Link href={props.youtube} rel="noopener noreferrer" target="_blank"><FaYoutube /></Link> }
-      { props.twitter && <Link href={props.twitter} rel="noopener noreferrer" target="_blank"><FaTwitter /></Link> }
-    </>
+export function ExternalLink({ type, url }: ExternalLinkProps) {
+  return (
+    <Link href={url} rel="noopener noreferrer" target="_blank">
+      {type === "website" && <FaSitemap />}
+      {type === "github" && <FaGithub />}
+      {type === "linkedin" && <FaLinkedinIn />}
+      {type === "googlePlay" && <FaGooglePlay />}
+      {type === "appleStore" && <FaApple />}
+      {type === "facebook" && <FaFacebookF />}
+      {type === "instagram" && <FaInstagram />}
+      {type === "youtube" && <FaYoutube />}
+      {type === "twitter" && <FaTwitter />}
+    </Link>
   );
 }
