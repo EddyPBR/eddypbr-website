f4b60b7b45da47b9eaac523d834d270f6cd7e638
EddyPBR <edvaldojunior1310@gmail.com>
2021-07-28 16:29:45 -0300

    :wrench: Adicionando botão de back page na página portfolio

--- src/pages/portfolio/[title].tsx
+++ src/pages/portfolio/[title].tsx
@@ -5,7 +5,7 @@ import { projects, ProjectType } from "../../data/projects";
 
 import { Badge } from "../../components/Badge";
 import { ExternalLink } from "../../components/ExternalLink";
-import { BackToHomeLink } from "../../components/BackToHomeLink";
+import { BackButton } from "../../components/BackButton";
 
 import { Container, ProjectHead, ProjectContent, ProjectBody, ProjectFooter } from "../../styles/Project";
 
@@ -52,7 +52,7 @@ export default function Letmeask({ project }: { project: ProjectType }) {
         </ProjectContent>
       </Container>
 
-      <BackToHomeLink />
+      <BackButton />
     </>
   );
 }
