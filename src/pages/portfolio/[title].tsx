ca1f39c811d707385b60db7a985c13af243ab291
EddyPBR <edvaldojunior1310@gmail.com>
2021-08-02 14:42:55 -0300

    :wrench: Refatorando página de projeto-portfolio e estilização

--- src/pages/portfolio/[title].tsx
+++ src/pages/portfolio/[title].tsx
@@ -1,15 +1,20 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 
+import { sanitize } from "isomorphic-dompurify";
 import { projects, ProjectType } from "../../data/projects";
 
 import { Badge } from "../../components/Badge";
 import { ExternalLink } from "../../components/ExternalLink";
 import { BackButton } from "../../components/BackButton";
 
-import { Container, ProjectHead, ProjectContent, ProjectBody, ProjectFooter } from "../../styles/Project";
+import { Container, PostHead, PostBody, ContentHead, ContentBody, ContentFooter } from "../../styles/Post";
 
 export default function Letmeask({ project }: { project: ProjectType }) {
+  const sanitizedContent = { 
+    __html: sanitize(project.content),
+  }
+
   return (
     <>
       <Head>
@@ -17,39 +22,37 @@ export default function Letmeask({ project }: { project: ProjectType }) {
       </Head>
 
       <Container>
-        <ProjectHead bgUrl={project.thumbnail} />
-        <ProjectContent>
-          <ProjectBody>
-            <h1>{project.title}</h1>
-            <div>
-              {
-                project.categories.map((category, index) => <Badge key={index} type={category} />)
-              }
-            </div>
-            {
-              project.content.map((data, index) => {
-                if (data.type === "text") {
-                  return <p key={index}>{data.data}</p>
+        <PostHead bgUrl={project.thumbnail} />
+        <PostBody>
+          <article>
+            <ContentHead>
+              <h1>{project.title}</h1>
+              <div>
+                {
+                  project.categories.map((category, index) => <Badge key={index} type={category} />)
                 }
-              })
-            }
-          </ProjectBody>
-          <ProjectFooter>
-            <div>
-              <strong>Equipe:</strong>
-              {
-                project.team.map((member, index) => <cite key={index}>&ndash; {member}</cite>)
-              }
-            </div>
-            <div>
-              {
-                Object.keys(project.otherLinks).map((atribute, index) =>
-                  <ExternalLink key={index} type={atribute} url={project.otherLinks[atribute]} />
-                )
-              }
-            </div>
-          </ProjectFooter>
-        </ProjectContent>
+              </div>
+            </ContentHead>
+
+            <ContentBody dangerouslySetInnerHTML={sanitizedContent} />
+
+            <ContentFooter>
+              <div>
+                <strong>Equipe:</strong>
+                {
+                  project.team.map((member, index) => <cite key={index}>&ndash; {member}</cite>)
+                }
+              </div>
+              <div>
+                {
+                  Object.keys(project.otherLinks).map((atribute, index) =>
+                    <ExternalLink key={index} type={atribute} url={project.otherLinks[atribute]} />
+                  )
+                }
+              </div>
+            </ContentFooter>
+          </article>
+        </PostBody>
       </Container>
 
       <BackButton />
