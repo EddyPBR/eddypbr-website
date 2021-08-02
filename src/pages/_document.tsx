55b53f85d74cc2e6f0ee227eef58e79725448928
EddyPBR <edvaldojunior1310@gmail.com>
2021-08-02 16:27:38 -0300

    :octocat: Alterando favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
