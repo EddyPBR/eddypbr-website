a3894bd0a4a9f1686cfed1cad0523e90464f9fa7
EddyPBR <edvaldojunior1310@gmail.com>
2021-08-02 13:09:21 -0300

    :wrench: Atualizando o _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <meta name="theme-color" content="#645CEE" />
         </Head>
         <body>
           <Main />
