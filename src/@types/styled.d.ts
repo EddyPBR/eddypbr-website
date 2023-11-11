470296fe40867e7eb13afe8b3c2209bf49a8ed51
EddyPBR <edvaldojunior1310@gmail.com>
2023-11-11 10:09:13 -0300

    :fire: remove all files

--- src/@types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import "styled-components";
-
-import { theme } from "../global/theme";
-
-export type Theme = typeof theme;
-
-declare module "styled-components" {
-  export interface DefaultTheme extends Theme {};
-};
--- src/utils/parseStringToDashedUrl.ts
+++
@@ -1,3 +0,0 @@
-export function parseStringToDashedUrl(word: string): string {
-  return word.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f:,?-]/g, '').split(" ").join("-");
-}
